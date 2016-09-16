7671ca550cda4700a400a17360d280abcd550401
Giancarlo Ventura <sakya_stelios@hotmail.com>
2016-09-16 11:41:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`No entiendo eso.`).then(() => 'speak');
+                    return bot.say(`Lo siento, parece que mi creador aun no me enseña cómo responder a tu pedido.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
